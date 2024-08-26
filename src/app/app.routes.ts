6b4b8b235b8f21e96793f03f1275c27775d43169
Ronellyson <ronellysonjulio@gmail.com>
2024-08-26 17:33:54 -0300

    Initial implementation of the congratulations screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HelpComponent } from './components/screens/help/help.component';
 import { HomeComponent } from './components/screens/home/home.component';
 import { StartComponent } from './components/screens/start/start.component';
 import { ContentCarouselComponent } from './components/screens/content-carousel/content-carousel.component';
+import { CongratulationsComponent } from './components/screens/congratulations/congratulations.component';
 
 export const routes: Routes = [
   { path: 'start', component: StartComponent },
   { path: 'home', component: HomeComponent },
   { path: 'help', component: HelpComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'congratulations', component: CongratulationsComponent},
   { path: 'phases/:id', component:ContentCarouselComponent},
   { path: '', redirectTo: '/start', pathMatch: 'full' }
 ];
--- src/app/components/screens/congratulations/congratulations.component.ts
+++ src/app/components/screens/congratulations/congratulations.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-congratulations',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgIf,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgClass,
+  ],
   templateUrl: './congratulations.component.html',
-  styleUrl: './congratulations.component.scss'
+  styleUrls: ['./congratulations.component.scss'],
 })
-export class CongratulationsComponent {
+export class CongratulationsComponent implements OnInit {
+  @Input() phaseTitle: string | undefined;
+  @Input() phaseNumber: number | undefined;
+  @Input() totalErrors: number = 0;
+  @Input() totalQuestions: number = 0;
 
+  congratulations_message = '';
+  starsArray: number[] = [];
+
+  constructor(private router: Router, private location: Location) {}
+
+  ngOnInit() {
+    this.congratulations_message = `Parabéns por ter completado a fase ${this.phaseNumber}. Aqui está sua pontuação:`;
+    this.calculateStars();
+  }
+
+  calculateStars(): void {
+    if (this.totalQuestions > 0) {
+      const errorPercentage = this.totalErrors / this.totalQuestions;
+      const maxStars = 5;
+      // Calcula o número de estrelas, garantindo um mínimo de 1 estrela
+      const stars = Math.max(1, Math.round((1 - errorPercentage) * maxStars));
+      this.starsArray = Array(stars).fill(0);
+    }
+  }
+
+  navigate(screen: string): void {
+    this.router.navigate([`/${screen}`]);
+  }
+
+  navigateBack() {
+    this.location.back();
+  }
 }
